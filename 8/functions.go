c743ac4b304ab6e342750f5971f54566e4f69e51
pourya ramezani <pourya.ramezani.21224@gmail.com>
2023-12-18 15:47:37 +0330

    update eighth file

--- 8/functions.go
+++ 8/functions.go
@@ -2,7 +2,8 @@ package main
 
 import "fmt"
 
-func power(num int, power int) int {
+//number 1
+func power(num, power int) int {
 	output := 1
 	for i := 0; i < power; i++ {
 		output = output * num
@@ -10,8 +11,20 @@ func power(num int, power int) int {
 	return output
 }
 
+//number 2
+func sum(nums ...int) (total int) {
+	total = 0
+
+	fmt.Print(nums, " =sum=> ")
+
+	for _, num := range nums {
+		total += num
+	}
+	return
+}
+
 func main() {
-	m := 5
-	p := 3
-	fmt.Println(power(m, p))
+	fmt.Println(power(5, 3))
+
+	fmt.Println(sum(45, 23, 56, 45, 78, 23, 45, 11, 6, 23, 4, 856, -100))
 }
