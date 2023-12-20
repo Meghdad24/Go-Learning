716f5e3c837ce26c2d415fdf0dfdcbf51f07eed8
pourya ramezani <pourya.ramezani.21224@gmail.com>
2023-12-20 15:10:04 +0330

    Booking program | adding fmt.Scan | Update Greeting part

--- Booking Program/tst.go
+++ Booking Program/tst.go
@@ -10,4 +10,21 @@ func main() {
 	fmt.Printf("Hello! Welcome to %v!\n", ConferenceName)
 	fmt.Printf("We have %v Ticket for sell.\n", ticketCounts-ticketSold)
 
+	var firstName, lastName string
+	fmt.Println("What's your name(Firstname & lastname)?")
+	fmt.Scan(&firstName)
+	fmt.Scan(&lastName)
+
+	var userEmail string
+	fmt.Println("Enter your email address:")
+	fmt.Scan(&userEmail)
+
+	var userTickets int
+	fmt.Println("How many tickets you want?")
+	fmt.Scan(&userTickets)
+
+	ticketSold += userTickets
+
+	fmt.Printf("Mr.%v! You have %v tickets and %v tickets are left for sale.", lastName, userTickets, ticketCounts-ticketSold)
+
 }
