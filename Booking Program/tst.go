50a0f485f0cfad995b593b4f6d52cabbd7c67cbb
pourya ramezani <pourya.ramezani.21224@gmail.com>
2023-12-20 15:44:55 +0330

    Booking program | Adding Array concept

--- Booking Program/tst.go
+++ Booking Program/tst.go
@@ -6,6 +6,7 @@ func main() {
 	ConferenceName := "Go Conference"
 	const ticketCounts int = 40
 	var ticketSold int = 0
+	var bookings [50]string
 
 	fmt.Printf("Hello! Welcome to %v!\n", ConferenceName)
 	fmt.Printf("We have %v Ticket for sell.\n", ticketCounts-ticketSold)
@@ -14,6 +15,7 @@ func main() {
 	fmt.Println("What's your name(Firstname & lastname)?")
 	fmt.Scan(&firstName)
 	fmt.Scan(&lastName)
+	bookings[0] = firstName + " " + lastName
 
 	var userEmail string
 	fmt.Println("Enter your email address:")
@@ -27,4 +29,9 @@ func main() {
 
 	fmt.Printf("Mr.%v! You have %v tickets and %v tickets are left for sale.", lastName, userTickets, ticketCounts-ticketSold)
 
+	fmt.Println("Array properties:")
+	fmt.Printf("The whole array: %v\n", bookings)
+	fmt.Printf("The first value: %v\n", bookings[0])
+	fmt.Printf("Array type: %T\n", bookings)
+	fmt.Printf("Size of array: %v", len(bookings))
 }
