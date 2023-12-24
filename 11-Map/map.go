1381782bdcfb18b5ac019ffda7ad1525a43747c4
pourya ramezani <pourya.ramezani.21224@gmail.com>
2023-12-24 21:13:01 +0330

    11th .go file | Updated

--- 11-Map/map.go
+++ 11-Map/map.go
@@ -12,6 +12,56 @@ func main() {
 	// myMap := make(map[int]string)
 	myKey := 13
 	myMap[myKey] = "thirteen"
-	fmt.Println(myMap)        //map[13:thirteen]
-	fmt.Println(myMap[myKey]) //thirteen
+	fmt.Println(myMap)               //map[13:thirteen]
+	fmt.Println(myMap[myKey] + "\n") //thirteen
+
+	myMap2 := map[string]map[int]bool{
+		"Sunday": {
+			1: true,
+			2: true,
+			3: true,
+			4: true,
+		},
+		"Monday": {
+			1: true,
+			2: true,
+			3: true,
+			4: true,
+		},
+		"Tuesday": {
+			1: true,
+			2: true,
+			3: true,
+			4: true,
+		},
+		"Wednesday": {
+			1: true,
+			2: true,
+			3: true,
+			4: true,
+		},
+		"Thursday": {
+			1: true,
+			2: true,
+			3: true,
+			4: true,
+		},
+		"Friday": {
+			1: true,
+			2: true,
+			3: true,
+			4: true,
+		},
+		"Saturday": {
+			1: true,
+			2: true,
+			3: true,
+			4: true,
+		},
+	}
+	for k, _ := range myMap2 {
+		fmt.Printf("%s ->	%v\n", k, myMap2[k])
+	}
+	fmt.Println()
+
 }
