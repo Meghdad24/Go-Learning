6a073c40ff617acc8fdcd08a7f35113632aa46d1
pourya ramezani <pourya.ramezani.21224@gmail.com>
2023-12-31 17:02:40 +0330

    14th .go file | defer 3

--- 14-defer_panic_recovery/deferPanicRecovery.go
+++ 14-defer_panic_recovery/deferPanicRecovery.go
@@ -9,7 +9,17 @@ import (
 // 	fmt.Println("hello")
 // }
 
+// func main() {
+// 	defer func() { fmt.Println("In inline defer") }()
+// 	fmt.Println("Executed")
+// }
+
 func main() {
-	defer func() { fmt.Println("In inline defer") }()
-	fmt.Println("Executed")
+	i := 0
+	i = 1
+	defer fmt.Println(i)
+	i = 2
+	defer fmt.Println(i)
+	i = 3
+	defer fmt.Println(i)
 }
