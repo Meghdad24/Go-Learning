e6a2bc196d701e5d2fd253957ddc732a73cb2288
pourya ramezani <pourya.ramezani.21224@gmail.com>
2023-12-31 17:03:51 +0330

    14th .go file | defer 4

--- 14-defer_panic_recovery/deferPanicRecovery.go
+++ 14-defer_panic_recovery/deferPanicRecovery.go
@@ -14,12 +14,20 @@ import (
 // 	fmt.Println("Executed")
 // }
 
+// func main() {
+// 	i := 0
+// 	i = 1
+// 	defer fmt.Println(i)
+// 	i = 2
+// 	defer fmt.Println(i)
+// 	i = 3
+// 	defer fmt.Println(i)
+// }
+
 func main() {
-	i := 0
-	i = 1
+	i := 1
 	defer fmt.Println(i)
-	i = 2
-	defer fmt.Println(i)
-	i = 3
-	defer fmt.Println(i)
+	i++
+	fmt.Println(i)
+	fmt.Println("First")
 }
