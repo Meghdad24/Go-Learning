8dc6ad5e419290c5350a0d2690ed34ea74cdd082
pourya ramezani <pourya.ramezani.21224@gmail.com>
2024-01-01 19:30:41 +0330

    14th .go file | panic 3

--- 14-defer_panic_recovery/deferPanicRecovery.go
+++ 14-defer_panic_recovery/deferPanicRecovery.go
@@ -43,15 +43,36 @@ import "fmt"
 // 	fmt.Println(a[index])
 // }
 
+// func main() {
+
+// 	a := []string{"a", "b"}
+// 	checkAndPrint(a, 2)
+// }
+
+// func checkAndPrint(a []string, index int) {
+// 	if index > (len(a) - 1) {
+// 		panic("Out of bound access for slice")
+// 	}
+// 	fmt.Println(a[index])
+// }
+
 func main() {
 
 	a := []string{"a", "b"}
 	checkAndPrint(a, 2)
+	fmt.Println("Exiting normally")
 }
 
 func checkAndPrint(a []string, index int) {
+	defer handleOutOfBounds()
 	if index > (len(a) - 1) {
 		panic("Out of bound access for slice")
 	}
 	fmt.Println(a[index])
+}
+
+func handleOutOfBounds() {
+	if r := recover(); r != nil {
+		fmt.Println("Recovering from panic:", r)
+	}
 }
